22c6f789962c555528a240b4abf101b27aa48952
yjs2952 <yjs2952@naver.com>
2020-10-19 18:03:55 +0900

    구현 (왕실 나이트)

--- ch04/04_3_knight.py
+++ ch04/04_3_knight.py
@@ -7,3 +7,11 @@ steps = [(-2, -1), (-1, -2), (1, -2), (2, -1), (2, 1), (1, 2), (-1, 2), (-2, 1)]
 
 result = 0
 
+for step in steps:
+    next_row = row + step[0]
+    next_column = column + step[1]
+
+    if 1 <= next_row <= 8 and 1 <= next_column <= 8:
+        result += 1
+
+print(result)
