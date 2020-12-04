e1406f145f27ddfe00fb4219ba70b838a08dd8fb
yjs2952 <yjs2952@naver.com>
2020-12-04 16:00:36 +0900

    금광 문제

---
+++ part02/ch08/09_mine.py
@@ -0,0 +1,38 @@
+t = int(input())
+answer = []
+for _ in range(t):
+    n, m = map(int, input().split())
+
+    array = list(map(int, input().split()))
+
+    dp = []
+    index = 0
+    for _ in range(n):
+        dp.append(array[index:index + m])
+        index += m
+
+    for j in range(1, m):
+        for i in range(n):
+            if i == 0:
+                left_up = 0
+            else:
+                left_up = dp[i - 1][j - 1]
+            if i == n - 1:
+                left_down = 0
+            else:
+                left_down = dp[i + 1][j - 1]
+            left = dp[i][j - 1]
+            dp[i][j] += max(left, left_down, left_up)
+    result = 0
+    for i in range(n):
+        result = max(result, dp[i][m - 1])
+
+    answer.append(result)
+
+print(answer)
+
+# 2
+# 3 4
+# 1 3 3 2 2 1 4 1 0 6 4 7
+# 4 4
+# 1 3 1 5 2 2 4 1 5 0 2 3 0 6 1 2
